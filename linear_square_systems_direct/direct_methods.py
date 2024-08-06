72cea24cb8e570b10d8ca93d2dfb5ba5cfcd7c57
gclembo <gcarterlembo@gmail.com>
2024-08-06 12:03:50 -0700

    Added start to complete GE, renamed variables

--- linear_square_systems_direct/direct_methods.py
+++ linear_square_systems_direct/direct_methods.py
@@ -3,21 +3,21 @@ import numpy as np
 
 # Given a upper triangular matrix, returns the solution vector if there is unique solution
 def back_substitution(matrix):
-    n = matrix.shape[0]
-    sol = np.array([None] * n)
-    for i in range(n - 1, -1, -1):
+    m = matrix.shape[0]
+    sol = np.array([None] * m)
+    for i in range(m - 1, -1, -1):
         total = 0
-        for j in range(n - 1, i, -1):
+        for j in range(m - 1, i, -1):
             total += matrix[i, j] * sol[j]
 
-        x = 1 # Default value
+        x = 1  # Default value
         if matrix[i, i] == 0:
-            if total == matrix[i, n]:
+            if total == matrix[i, m]:
                 print("x" + str(i) + " is free parameter. Using 1 as default.")
             else:
                 raise ValueError("No solutions")
         else:
-            x = (matrix[i, n] / matrix[i, i]) - total
+            x = (matrix[i, m] / matrix[i, i]) - total
         sol[i] = float(x)
     return sol
 
@@ -25,19 +25,36 @@ def back_substitution(matrix):
 # Given a regular matrix, performs regular gaussian elimination. Raises ValueError
 #  if given matrix is not regular
 def normal_gaussian_elim(in_matrix):
-    n = in_matrix.shape[0]
+    m = in_matrix.shape[0]
     out_matrix = in_matrix.copy()
-    for i in range(n - 1):
+    for i in range(m - 1):
         if out_matrix[i][i] == 0:
             raise ValueError("Given matrix is not regular")
 
-        for j in range(i + 1, n):
+        for j in range(i + 1, m):
             coef = out_matrix[j][i] / out_matrix[i][i]
             out_matrix[j] = out_matrix[j] - coef * out_matrix[i]
-
     return out_matrix
 
 
-system = np.array([[1, 1, 1, 1], [0, 1, 1, 1], [0, 0, 1, 0]])
+def complete_gaussian_elim(in_matrix):
+    m = in_matrix.shape[0]
+    out_matrix = in_matrix.copy()
+    for i in range(m - 1):
+        row = i + 1
+        while out_matrix[i][i] == 0 and row != m:
+            out_matrix[[i, row]] = out_matrix[[row, i]]
+            row += 1
+
+        if out_matrix[i][i] == 0:
+            continue
+
+        for j in range(i + 1, m):
+            coef = out_matrix[j][i] / out_matrix[i][i]
+            out_matrix[j] = out_matrix[j] - coef * out_matrix[i]
+    return out_matrix
+
+
+system = np.array([[0, 0, 0, 1], [1, 0, 1, 1], [1, 0, 1, 0]])
 print(system)
-print(back_substitution(system))
+print(complete_gaussian_elim(system))
