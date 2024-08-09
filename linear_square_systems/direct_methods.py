70580541bb583dc18a4570aaa9ff5db38df0d0d9
gclembo <gcarterlembo@gmail.com>
2024-08-09 11:17:18 -0700

    renamed variables and cleaned code

--- linear_square_systems/direct_methods.py
+++ linear_square_systems/direct_methods.py
@@ -1,81 +1,73 @@
 import numpy as np
 
 
-# Given a upper triangular matrix, returns the solution vector if there is unique solution
-def back_substitution(matrix):  # TODO change to add b vector
-    m = matrix.shape[0]
+# Given a upper triangular augmented a vector , returns the solution vector if there is unique solution
+def back_substitution(upper, b):
+    m = upper.shape[0]
+    augmented = np.array([upper, b])
     sol = np.array([None] * m)
     for i in range(m - 1, -1, -1):
         total = 0
         for j in range(m - 1, i, -1):
-            total += matrix[i, j] * sol[j]
+            total += augmented[i, j] * sol[j]
 
         x = 1  # Default value
-        if matrix[i, i] == 0:
-            if total == matrix[i, m]:
+        if augmented[i, i] == 0:
+            if total == augmented[i, m]:
                 print("x" + str(i) + " is free parameter. Using 1 as default.")
             else:
                 raise ValueError("No solutions")
         else:
-            x = (matrix[i, m] / matrix[i, i]) - total
+            x = (augmented[i, m] / augmented[i, i]) - total
         sol[i] = float(x)
     return sol
 
 
-# Given a regular matrix, performs regular gaussian elimination. Raises ValueError
-#  if given matrix is not regular
-def regular_gaussian_elim(in_matrix):
-    m = in_matrix.shape[0]
-    out_matrix = in_matrix.copy()
+# Given a regular augmented, performs regular gaussian elimination. Raises ValueError
+#  if given augmented is not regular
+def regular_gaussian_elim(input_matrix):
+    m = input_matrix.shape[0]
+    reduced = input_matrix.copy()
     for i in range(m - 1):
-        if out_matrix[i][i] == 0:
-            raise ValueError("Given matrix is not regular")
+        if reduced[i][i] == 0:
+            raise ValueError("Given augmented is not regular")
 
         for j in range(i + 1, m):
-            coef = out_matrix[j][i] / out_matrix[i][i]
-            out_matrix[j] = out_matrix[j] - coef * out_matrix[i]
-    return out_matrix
+            coef = reduced[j][i] / reduced[i][i]
+            reduced[j] = reduced[j] - coef * reduced[i]
+    return reduced
 
 
-def complete_gaussian_elim(in_matrix):
-    m = in_matrix.shape[0]
-    out_matrix = in_matrix.copy()
+def complete_gaussian_elim(input_matrix):
+    m = input_matrix.shape[0]
+    reduced = input_matrix.copy()
     for i in range(m - 1):
         row = i + 1
-        while out_matrix[i][i] == 0 and row != m:
-            out_matrix[[i, row]] = out_matrix[[row, i]]
+        while reduced[i][i] == 0 and row != m:
+            reduced[[i, row]] = reduced[[row, i]]
             row += 1
 
-        if out_matrix[i][i] == 0:
+        if reduced[i][i] == 0:
             continue
 
         for j in range(i + 1, m):
-            coef = out_matrix[j][i] / out_matrix[i][i]  # TODO check for no sols
-            out_matrix[j] = out_matrix[j] - coef * out_matrix[i]
-    return out_matrix
+            coef = reduced[j][i] / reduced[i][i]
+            reduced[j] = reduced[j] - coef * reduced[i]
+    return reduced
 
 
-# Given a regular square matrix, decomposes the matrix into lower and upper
+# Given a regular square augmented, decomposes the augmented into lower and upper
 #  triangular matrices
-def lu_decomposition(in_matrix):
-    m = in_matrix.shape[0]
+def lu_decomposition(input_matrix):
+    m = input_matrix.shape[0]
     l = np.eye(m)
-    u = in_matrix.copy()
+    u = input_matrix.copy()
     for i in range(m - 1):
         if u[i][i] == 0:
-            raise ValueError("Given matrix is not regular")
+            raise ValueError("Given argumented is not regular")
         for j in range(i + 1, m):
             coef = u[j][i] / u[i][i]
             l[j][i] = coef
             u[j] = u[j] - coef * u[i]
     return l, u
 
-
-system = np.array([[1, 2, 3], [2, 3, 4], [1, 1, 2]])
-temp = lu_decomposition(system)
-l = temp[0]
-u = temp[1]
-print(l)
-print(u)
-print(system)
-print(np.matmul(l, u))
