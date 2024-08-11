9f105d6a1239ef25b2f2cedfd1172796ac36cdf0
gclembo <gcarterlembo@gmail.com>
2024-08-11 10:55:30 -0700

    added tests for regular GE

--- linear_square_systems/direct_methods_tests.py
+++ linear_square_systems/direct_methods_tests.py
@@ -10,7 +10,7 @@ def test_back_substitution():
     )
     b = np.array([1, 2, 3]).transpose()
     sol = np.array([1, 2, 3])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 1 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 1 Error")
 
     a = np.array(
         [[1, 1, 1],
@@ -18,7 +18,7 @@ def test_back_substitution():
          [0, 0, 1]]
     )
     sol = np.array([-1, -1, 3])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 2 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 2 Error")
 
     a = np.array([
         [3, 2, 1],
@@ -26,15 +26,15 @@ def test_back_substitution():
         [0, 0, 3]]
     )
     sol = np.array([0, 0, 1])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 3 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 3 Error")
 
     b = np.array([0, 0, 0]).transpose()
     sol = np.array([0, 0, 0])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 4 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 4 Error")
 
     b = np.array([1, 1, 1]).transpose()
     sol = np.array([4 / 27, 1 / 9, 1 / 3])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 5 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 5 Error")
 
     a = np.array(
         [[1, 2],
@@ -42,7 +42,7 @@ def test_back_substitution():
     )
     b = np.array([2, 3]).transpose()
     sol = np.array([8 / 7, 3 / 7])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 6 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 6 Error")
 
     a = np.array(
         [[1, 2],
@@ -50,7 +50,7 @@ def test_back_substitution():
     )
     b = np.array([1, 0]).transpose()
     sol = np.array([1, 0])
-    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg='Back Sub Test 7 Error')
+    np.testing.assert_almost_equal(direct_methods.back_substitution(a, b), sol, err_msg="Back Sub Test 7 Error")
 
     # check error if not upper triangular
     a = np.array(
@@ -84,6 +84,79 @@ def test_back_substitution():
     np.testing.assert_raises(ValueError, direct_methods.back_substitution, a, b)
 
 
+def test_regular_gaussian_elimination():
+    a = np.array(
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]]
+    )
+    sol = np.array(
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]]
+    )
+    np.testing.assert_almost_equal(direct_methods.regular_gaussian_elim(a), sol, err_msg="Regular GE Test 1 Fail")
+
+    a = np.array(
+        [[1, 2, 3],
+         [1, 3, 4],
+         [1, 1, 1]]
+    )
+    sol = np.array(
+        [[1, 2, 3],
+         [0, 1, 1],
+         [0, 0, -1]]
+    )
+    np.testing.assert_almost_equal(direct_methods.regular_gaussian_elim(a), sol, err_msg="Regular GE Test 2 Fail")
+
+    a = np.array(
+        [[1, 2, 3],
+         [1, 1, 1],
+         [1, 1, 1]]
+    )
+    sol = np.array(
+        [[1, 2, 3],
+         [0, -1, -2],
+         [0, 0, 0]]
+    )
+    np.testing.assert_almost_equal(direct_methods.regular_gaussian_elim(a), sol, err_msg="Regular GE Test 3 Fail")
+
+    a = np.array(
+        [[1, 2],
+         [1, 3],
+         [1, 1]]
+    )
+    sol = np.array(
+        [[1, 2],
+         [0, 1],
+         [0, 0]]
+    )
+    np.testing.assert_almost_equal(direct_methods.regular_gaussian_elim(a), sol, err_msg="Regular GE Test 4 Fail")
+
+    a = np.array(
+        [[1, 2, 3, 4],
+         [1, 3, 4, 5],
+         [1, 1, 1, 1]]
+    )
+    sol = np.array(
+        [[1, 2, 3, 4],
+         [0, 1, 1, 1],
+         [0, 0, -1, -2]]
+    )
+    np.testing.assert_almost_equal(direct_methods.regular_gaussian_elim(a), sol, err_msg="Regular GE Test 5 Fail")
+
+    # Tests for error with non regular matrix
+    a = np.array(
+        [[1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1]]
+    )
+    np.testing.assert_raises(ValueError, direct_methods.regular_gaussian_elim, a)
+
+
 if __name__ == "__main__":
     test_back_substitution()
+    print("Back sub passed")
+    test_regular_gaussian_elimination()
+    print("Regular GE passed")
     print("Tests Passed!")
