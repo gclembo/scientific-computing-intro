b1822fbed2c87b6b2d7b5837e5607038eb0fcfe3
gclembo <gcarterlembo@gmail.com>
2024-08-12 09:36:51 -0700

    renamed variables and edited comments

--- linear_square_systems/direct_methods.py
+++ linear_square_systems/direct_methods.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-# Given a upper triangular augmented a vector , returns the solution vector if there is unique solution
+# Given a upper triangular matrix a vector , returns the solution vector if there is unique solution
 def back_substitution(upper: np.array, b: np.array) -> np.array:
     if upper.shape[0] != upper.shape[1]:
         raise ValueError("System is not square")
@@ -31,7 +31,7 @@ def back_substitution(upper: np.array, b: np.array) -> np.array:
 
 
 # Given a regular matrix, performs regular gaussian elimination. Raises ValueError
-#  if given augmented is not regular
+#  if given matrix is not regular
 def regular_gaussian_elim(input_matrix: np.array) -> np.array:
     m = input_matrix.shape[0]
     reduced = input_matrix.astype(np.float32)
@@ -53,14 +53,13 @@ def complete_gaussian_elim(input_matrix: np.array) -> np.array:
     curr_col = 0
     while curr_row < m and curr_col < n:
         swap_row = curr_row + 1
-        while reduced[curr_row][curr_col] == 0 and swap_row != m:
+        while reduced[curr_row][curr_col] == 0 and swap_row < m:
             reduced[[curr_row, swap_row]] = reduced[[swap_row, curr_row]]
             swap_row += 1
 
-        if reduced[curr_row][curr_col] == 0:
+        if swap_row == m:
             curr_col += 1
             continue
-
 
         for j in range(curr_row + 1, m):
             coef = reduced[j][curr_col] / reduced[curr_row][curr_col]
@@ -70,8 +69,8 @@ def complete_gaussian_elim(input_matrix: np.array) -> np.array:
     return reduced
 
 
-# Given a regular square augmented, decomposes the augmented into lower and upper
-#  triangular matrices
+# Given a regular square matrix, decomposes the matrix into lower and upper
+#  triangular matrices and returns the lower and upper matrices.
 def lu_decomposition(input_matrix: np.array) -> np.array:
     m = input_matrix.shape[0]
     l = np.eye(m)
