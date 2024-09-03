a77000785846b6909e65934e39dfb28cb466456f
gclembo <gcarterlembo@gmail.com>
2024-09-02 21:29:15 -0700

    Added root finding algorithms and slight edits to tests

--- optimization_and_root_finding/root_finding.py
+++ optimization_and_root_finding/root_finding.py
@@ -17,6 +17,33 @@ def bisection_method(f: callable(float), a: float, b: float,
     :param steps: Number of steps until algorythm stops iterating. Defaults to 50.
     :return: Approximate x value of root in the interval.
     """
+    if a >= b:
+        raise ValueError("First endpoint must be less than second endpoint")
+    f_a = f(a)
+    f_b = f(b)
+
+    if np.sign(f_a) == np.sign(f_b):
+        raise ValueError("The function evaluated at the given endpoints must have different signs")
+    if f_a == 0:
+        return a
+    if f_b == 0:
+        return b
+    iteration = 0
+    dist = (b - a) / 2
+    while iteration < steps and threshold < dist:
+        mid = (b + a) / 2
+        f_mid = f(mid)
+        if np.sign(f_a) == np.sign(f_mid):
+            a = mid
+            f_a = f_mid
+        elif np.sign(f_mid) == np.sign(f_b):
+            b = mid
+            f_b = f_mid
+        else:
+            return mid
+        iteration += 1
+        dist /= 2
+    return (a + b) / 2
 
 
 def newton_method(f: callable(float), df: callable(float), x0: float,
@@ -35,6 +62,17 @@ def newton_method(f: callable(float), df: callable(float), x0: float,
     :param steps: Number of steps until algorythm stops iterating. Defaults to 50.
     :return: Approximate x value of root in the interval.
     """
+    if df(x0) == 0:
+        raise ValueError("Starting point must not have a derivative of 0")
+    x1 = x0 - f(x0) / df(x0)
+    iteration = 1
+    dist = np.abs(x1 - x0)
+    while iteration < steps and threshold < dist:
+        x0 = x1
+        x1 = x0 - f(x0) / df(x0)
+        iteration += 1
+        dist = np.abs(x1 - x0)
+    return x1
 
 
 def secant_method(f: callable(float), x0: float, x1: float,
@@ -52,3 +90,15 @@ def secant_method(f: callable(float), x0: float, x1: float,
     :param steps: Number of steps until algorythm stops iterating. Defaults to 50.
     :return: Approximate x value of root in the interval.
     """
+    if f(x0) == f(x1):
+        raise ValueError("The function evaluated at the given points must have different values")
+    x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+    iteration = 1
+    dist = np.abs(x2 - x1)
+    while iteration < steps and threshold < dist:
+        x0 = x1
+        x1 = x2
+        x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+        iteration += 1
+        dist = np.abs(x2 - x1)
+    return x2
--- optimization_and_root_finding/root_finding_tests.py
+++ optimization_and_root_finding/root_finding_tests.py
@@ -7,19 +7,23 @@ def bisection_method_tests():
 
     np.testing.assert_almost_equal(
         root_finding.bisection_method(f, 0, 2, steps=1),
-        3 / 2
+        3 / 2,
+        err_msg="Bisection Method Test 1 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.bisection_method(f, 0, 2, steps=2),
-        5 / 4
+        5 / 4,
+        err_msg="Bisection Method Test 2 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.bisection_method(f, 0, 2, steps=3),
-        11 / 8
+        11 / 8,
+        err_msg="Bisection Method Test 3 Fail"
     )
     np.testing.assert_almost_equal(
-        root_finding.bisection_method(f, 0, 2),
-        1.2471795724
+        root_finding.bisection_method(f, 0, 2, threshold=1e-7),
+        1.324717957244746,
+        err_msg="Bisection Method Test 4 Fail"
     )
     np.testing.assert_raises(
         ValueError,
@@ -40,24 +44,28 @@ def newton_method_tests():
 
     np.testing.assert_almost_equal(
         root_finding.newton_method(f, df, 1, steps=1),
-        3 / 2
+        3 / 2,
+        err_msg="Newton\'s Method Test 1 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.newton_method(f, df, 1, steps=2),
-        31 / 23
+        31 / 23,
+        err_msg="Newton\'s Method Test 2 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.newton_method(f, df, 1, steps=3),
-        71749 / 54142
+        71749 / 54142,
+        err_msg="Newton\'s Method Test 3 Fail"
     )
     np.testing.assert_almost_equal(
-        root_finding.bisection_method(f, 0, 2),
-        1.2471795724
+        root_finding.bisection_method(f, 0, 2, threshold=1e-7),
+        1.324717957244746,
+        err_msg="Newton\'s Method Test 4 Fail"
     )
     np.testing.assert_raises(
         ValueError,
         root_finding.newton_method,
-        f, df, np.sqrt(1/3), 0.1, 5
+        f, df, np.sqrt(1 / 3), 0.1, 5
     )
 
 
@@ -66,22 +74,36 @@ def secant_method_tests():
 
     np.testing.assert_almost_equal(
         root_finding.secant_method(f, 1, 3 / 2, steps=1),
-        19 / 15
+        19 / 15,
+        err_msg="Secant Method Test 1 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.secant_method(f, 1, 3 / 2, steps=2),
-        5631 / 4279
+        5631 / 4279,
+        err_msg="Secant Method Test 2 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.secant_method(f, 1, 3 / 2, steps=3),
-        1.3252
+        1.32521411396,
+        err_msg="Secant Method Test 3 Fail"
     )
     np.testing.assert_almost_equal(
         root_finding.secant_method(f, 1, 3 / 2),
-        1.2471795724
+        1.324717957244746,
+        err_msg="Secant Method Test 4 Fail"
     )
     np.testing.assert_raises(
         ValueError,
         root_finding.secant_method,
         f, -1, 0, 0.1, 5
     )
+
+
+if __name__ == '__main__':
+    bisection_method_tests()
+    print("Bisection Method Tests Passed")
+    newton_method_tests()
+    print("Newton\'s Method Tests Passed")
+    secant_method_tests()
+    print("Secant Method Tests Passed")
+    print("Tests Passed")
